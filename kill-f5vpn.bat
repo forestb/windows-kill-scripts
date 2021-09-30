29f7438d889a28c7f5c8e4b99b6228331ca98dd4
forestb <forest.balk@gmail.com>
2021-09-29 19:08:17 -0600

    Added first two scripts

---
+++ kill-f5vpn.bat
@@ -0,0 +1,15 @@
+setlocal EnableDelayedExpansion
+set "imgname=f5vpn.exe"
+set "opt=tokens=* delims="
+echo:
+set /a "count=0"
+for /f "%opt%" %%i in ('tasklist /fi "imagename eq !imgname!" /fi "status eq running"') do (
+    echo %%i
+    set /a "count+=1"
+)
+set /a "count-=2"
+echo:
+echo There are !count! !imgname! processes running.
+echo:
+if "!count!" gtr "0" (call taskkill /f /fi "imagename eq !imgname!")
+endlocal
